6185ca556b1a78097443fb7cce38289a8ba9a800
Stephen Capper <stephencapper@gmail.com>
2024-02-19 17:29:10 -0800

    complete schema and save function in database/index.js

--- database/index.js
+++ database/index.js
@@ -1,16 +1,47 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+const RepoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  node_id: String,
+  name: String,
+  html_url: String,
+  stargazers_count: Number
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+const ReposSchema = new mongoose.Schema({
+  //TODO: my schema here!
+  username: String,
+  repos: [RepoSchema]
+});
 
-let save = (/* TODO */) => {
+const Repos = mongoose.model('Repos', ReposSchema);
+
+const save = (username, repos, callback) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-}
+  // remove duplicates from repos, and update repo objects to only store required properties
+  let mappedRepos = repos.map((repo) => {
+    let { node_id, name, html_url, stargazers_count } = repo;
+    return { node_id, name, html_url, stargazers_count }
+  });
+  let idOnlyRepos = mappedRepos.map((repo) => {
+    return repo.node_id;
+  });
+  let filteredRepos = mappedRepos.filter((repo, index) => {
+    return idOnlyRepos.indexOf(repo.node_id) === index;
+  });
+
+  // create instance of Repos
+  let userRepos = new Repos;
+  //add username property
+  userRepos.username = username;
+  //iterate over filtered repos and add each to repos property of userRepos
+  for (let repo of filteredRepos) {
+    userRepos.repos.push(repo);
+  }
+  userRepos.save(callback);
+};
 
 module.exports.save = save;
