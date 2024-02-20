2509ef913b42a8dec7ff34e66c38e4e9e7fc1b07
Stephen Capper <stephencapper@gmail.com>
2024-02-20 11:18:40 -0800

    refactor database.js so that save does not add duplicate repos if same username is used

--- database/index.js
+++ database/index.js
@@ -5,8 +5,11 @@ const RepoSchema = new mongoose.Schema({
   // TODO: your schema here!
   node_id: String,
   name: String,
+  full_name: String,
   html_url: String,
-  stargazers_count: Number
+  stargazers_count: Number,
+  forks_count: Number,
+  owner_login: String
 });
 
 const ReposSchema = new mongoose.Schema({
@@ -21,10 +24,14 @@ const save = (username, repos, callback) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+
+
   // remove duplicates from repos, and update repo objects to only store required properties
   let mappedRepos = repos.map((repo) => {
-    let { node_id, name, html_url, stargazers_count } = repo;
-    return { node_id, name, html_url, stargazers_count }
+    let { node_id, name, full_name, html_url, stargazers_count, forks_count, owner } = repo;
+    let owner_login = owner.login;
+    return { node_id, name, full_name, html_url, stargazers_count, forks_count, owner_login }
   });
   let idOnlyRepos = mappedRepos.map((repo) => {
     return repo.node_id;
@@ -32,16 +39,24 @@ const save = (username, repos, callback) => {
   let filteredRepos = mappedRepos.filter((repo, index) => {
     return idOnlyRepos.indexOf(repo.node_id) === index;
   });
-
-  // create instance of Repos
-  let userRepos = new Repos;
-  //add username property
-  userRepos.username = username;
-  //iterate over filtered repos and add each to repos property of userRepos
-  for (let repo of filteredRepos) {
-    userRepos.repos.push(repo);
-  }
-  userRepos.save(callback);
+  // find user's repos already in database if exists
+  Repos.findOne({ username }, (err, doc) => {
+    if (err) {
+      //create new instance of Repos for user
+      let userRepos = new Repos;
+      //add username property
+      userRepos.username = username;
+      //iterate over filtered repos and add each to repos property of userRepos
+      for (let repo of filteredRepos) {
+        userRepos.repos.push(repo);
+      }
+      userRepos.save(callback);
+      return;
+    }
+    //if user already exists, replace the repo subdocuments for user with latest ones
+    doc.repos = repos;
+    doc.save(callback);
+  });
 };
 
 module.exports.save = save;
